4c8b236ac9a87075f3766f950fbeb7e2671de338
Oleg Alieksieienko <oleg.alieksieienko@taxify.eu>
2020-01-28 18:00:04 +0200

    --wip-- [skip ci]

--- dist/index.js
+++ dist/index.js
@@ -4747,7 +4747,10 @@ async function main() {
 
     // const commits = await runShellCommand(`git log --pretty=oneline --no-merges origin/${inputs.targetBranch}..${process.env.GITHUB_REF}`);
 
-    const commits = await git.log(`origin/${inputs.targetBranch}`, process.env.GITHUB_REF)
+    const commits = await git.log({
+      from: `origin/${inputs.targetBranch}`,
+      to: process.env.GITHUB_REF
+    })
     core.info(`Commits: ${commits}`);
   } catch (error) {
     core.debug(inspect(error));
