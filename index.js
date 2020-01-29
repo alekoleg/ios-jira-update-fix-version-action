2df941f4022a7d70e4d04c31444ca716e88b39cb
Oleg Alieksieienko <oleg.alieksieienko@taxify.eu>
2020-01-29 13:44:29 +0200

    --wip-- [skip ci]

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function main() {
 
     const commits = await git.log([
       `--no-merges`,
-      `origin/${inputs.targetBranch}..${process.env.GITHUB_REF}`,
+      `${inputs.targetBranch}..${process.env.GITHUB_REF}`,
     ])
     core.info("Commits: " + JSON.stringify(commits));
   } catch (error) {
